f0a30bc31d8e8888f8352e41da3a7cd80ffc5008
JacekGos <jacekg93@gmail.com>
2026-04-11 15:13:54 +0200

    KAN-8 initialized spring bood kotlin project

---
+++ src/test/kotlin/com/iiot/connector/ConnectorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.iiot.connector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConnectorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
